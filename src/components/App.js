f0a7f17526be76667d96d07f922aed0849b25640
Kevin Muriuki Karimi <kevimuri24@gmail.com>
2024-08-25 22:39:44 +0300

    Finished Code Challenge

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,38 @@
 import React, { useState } from "react";
-
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  const handleChangeType = (type) => {
+    setFilters({ type });
+  };
+
+  const handleFindPetsClick = () => {
+    let url = "http://localhost:3000/pets";
+
+    if (filters.type !== "all") {
+      url += `?type=${filters.type}`;
+    }
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setPets(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching pets:", error);
+      });
+  };
+
+  const handleAdoptPet = (id) => {
+    const updatedPets = pets.map((pet) =>
+      pet.id === id ? { ...pet, isAdopted: true } : pet
+    );
+    setPets(updatedPets);
+  };
 
   return (
     <div className="ui container">
@@ -15,10 +42,13 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters
+              onChangeType={handleChangeType}
+              onFindPetsClick={handleFindPetsClick}
+            />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={handleAdoptPet} />
           </div>
         </div>
       </div>
